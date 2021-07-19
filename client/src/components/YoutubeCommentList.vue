c10b4b8c9fdb9f2d0ab0b0341ddf23bb4079fb34
moguri846 <dkffkekahsl1@naver.com>
2021-07-19 21:56:21 +0900

    chore: 컴포넌트 추가

--- client/src/components/YoutubeCommentList.vue
+++ client/src/components/YoutubeCommentList.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <YoutubeInsertComment :data="commentList"></YoutubeInsertComment>
     <ul class="comment-list">
       <li v-for="(comment, index) in commentList" :key="index" class="comment-item">
         <YoutubeCommentItem :comment="comment"></YoutubeCommentItem>
@@ -11,19 +12,25 @@
 <script>
 import { mapState } from 'vuex';
 
+import YoutubeInsertComment from './YoutubeInsertComment.vue';
 import YoutubeCommentItem from './YoutubeCommentItem.vue';
 
 export default {
   computed: {
     ...mapState(['commentList']),
   },
+
   components: {
+    YoutubeInsertComment,
     YoutubeCommentItem,
   },
 };
 </script>
 
 <style scoped>
+.comment-list {
+  margin-top: 30px;
+}
 .comment-item {
   margin-bottom: 20px;
 }
---
+++ client/src/components/YoutubeInsertComment.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="insert-comment">
+    <span class="total-comment">댓글 {{ totalComment }}개</span>
+    <div>
+      <i class="fas fa-user"></i>
+      <input type="text" placeholder="공개 댓글 추가..." />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Array,
+  },
+  computed: {
+    totalComment() {
+      return this.data.length;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.insert-comment {
+  height: 88px;
+}
+.insert-comment > div {
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+}
+.insert-comment > div > .fas {
+  font-size: 55px;
+  padding: 0px 15px 15px 0px;
+}
+.insert-comment > div > input {
+  width: 100%;
+  border: none;
+  outline: none;
+  font-size: 16px;
+  border-bottom: 1px solid #909090;
+  background-color: #f9f9f9;
+}
+</style>
